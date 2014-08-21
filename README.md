09d35cf85f0f9a479484c6b125f8c3a4010e7686
Kahlil Lechelt <kahlil.lechelt@etecture.de>
2014-08-21 15:29:08 +0200

    edits

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-_Nothing to see here._
+# DalekJS
+
+## Installation
+
+```
+npm install dalek-cli -g
+```
+
+```
+npm install dalekjs --save-dev
+```
+
+```
+npm install dalek-browser-chrome --save-dev
+```
+
+```
+npm install dalek-browser-ie --save-dev
+```
+
+```
+npm install dalek-reporter-html --save-dev
+```
+
+```
+npm install dalek-browser-firefox --save-dev
+```
+
+```
+npm install dalek-browser-ios --save-dev
+```
+
+
+## Canary
+
+```
+npm install dalekjs-canary --save-dev
+```
+
+```
+npm install dalek-reporter-html-canary --save-dev
+```
--- test/03-kundenservice.js
+++ test/03-kundenservice.js
@@ -2,12 +2,19 @@ module.exports = {
   'Kundenservice': function (test) {
     test.open('https://www.ing-diba.de/girokonto/kundenservice/')
       .waitForElement('.gs-span-17 details:nth-child(1)')
-      .click('.gs-span-17 details:nth-child(1) summary:nth-child(1)')
-      .assert.visible('.gs-span-17 details:nth-child(1) div', 'Details are visible.')
+      // .click('.gs-span-17 details:nth-child(1) summary:nth-child(1)')
+      // .assert.visible('.gs-span-17 details:nth-child(1) div', 'Details are visible.')
       .assert.text('.gs-span-17 details:nth-child(1) div p:nth-child(1)', 'Sie können das Girokonto schnell und direkt online, telefonisch oder schriftlich per Brief als Einzel- oder Gemeinschaftskonto beantragen.', 'Text is there.')
-      .click('.gs-span-17 details:nth-child(1) summary:nth-child(1)')
-      .wait(10000)
-      .assert.notVisible('.gs-span-17 details:nth-child(1) div', 'Details are not visible anymore.')
+      // .click('.gs-span-17 details:nth-child(1) summary:nth-child(1)')
+      .click('details#01571 summary')
+      .assert.attr('details#01571', 'open', null, 'Detail is open')
       .done();
+  },
+
+  'Baufinanzierung': function (test) {
+      test.open('https://www.ing-diba.de/diba-forms/baufinanzierung/anfordern/?1&version=1')
+        .click('#id7')
+        .waitForElement('#id3e')
   }
+
 };
